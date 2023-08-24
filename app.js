463dcfafc4e4293b986d6e068dd0ee892daa0d7e
Vinayak <vinayakg236@gmail.com>
2023-08-24 23:08:01 +0530

    added funtions for insertion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const port = 420;
 const app = express();
 const authRoute = require("./auth/usercontroller");
-
+const ctrl = require("./db_controllers/invoivecontroller");
 app.listen(port, () => {
   console.log(`it's running on http://localhost:${port}`);
 });
@@ -10,3 +10,69 @@ app.listen(port, () => {
 app.use(express.json());
 
 app.use("/auth", authRoute);
+
+// Handle POST request to insert an invoice
+app.post("/insertInvoice", (req, res) => {
+  const { custName, gstIN, Invoice } = req.body;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).json({ error: "Failed to connect to the database" });
+    } else {
+      connection.beginTransaction(async (transactionError) => {
+        if (transactionError) {
+          connection.release();
+          res.status(500).json({ error: "Transaction initialization error" });
+        } else {
+          try {
+            // Insert into customers table
+            await insertIntoCustomersTable(
+              connection,
+              custName,
+              "",
+              "",
+              "",
+              "",
+              gstIN
+            );
+
+            // Insert into invoices table
+            const { insertId: customer_id } = await insertIntoInvoicesTable(
+              connection,
+              custName,
+              0, // Default values for customer_id and transaction_id
+              "", // These values should be generated or provided in the JSON
+              Invoice.date,
+              Invoice.amount,
+              Invoice.paymentmode
+            );
+
+            // Insert into invoice lines table for each item
+            for (const item of Invoice.invoiceitems) {
+              await insertIntoInvoiceLinesTable(
+                connection,
+                customer_id,
+                0, // Default value for invoice_id
+                0, // Default value for product_id
+                item.itemName,
+                item.rate,
+                item.qty,
+                item.rate * item.qty,
+                0, // Default value for tax
+                0 // Default value for taxed_amount
+              );
+            }
+
+            connection.commit();
+            connection.release();
+            res.status(200).json({ message: "Invoice inserted successfully" });
+          } catch (insertError) {
+            connection.rollback();
+            connection.release();
+            res.status(500).json({ error: "Invoice insertion error" });
+          }
+        }
+      });
+    }
+  });
+});
--- db_controllers/invoivecontroller.js
+++ db_controllers/invoivecontroller.js
@@ -1,19 +1,20 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
+const invoiceRouter = require("express").Router();
 
-const createCustomersTable = async (client, username) => {
+async function createCustomersTable(client, username) {
   const sql = `CREATE TABLE IF NOT EXISTS ${username}_customers (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
     phone_number VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    gstIN VARCHAR(255) NOT NULL,
+    gstIN VARCHAR(255) NOT NULL UNIQUE,
     PRIMARY KEY (id)
   );`;
   await client.query(sql);
-};
+}
 
-const createInvoicesTable = async (client, customerName) => {
+async function createInvoicesTable(client, customerName) {
   const sql = `CREATE TABLE IF NOT EXISTS ${customerName}_transaction (
     id INT NOT NULL AUTO_INCREMENT,
     customer_id INT NOT NULL,
@@ -25,9 +26,9 @@ const createInvoicesTable = async (client, customerName) => {
     FOREIGN KEY (customer_id) REFERENCES ${username}_customers (id)
   );`;
   await client.query(sql);
-};
+}
 
-const createInvoiceLinesTable = async (client, transactionID) => {
+async function createInvoiceLinesTable(client, transactionID) {
   const sql = `CREATE TABLE IF NOT EXISTS ${transactionID}_invoicelines (
     id INT NOT NULL AUTO_INCREMENT,
     invoice_id INT NOT NULL,
@@ -42,22 +43,90 @@ const createInvoiceLinesTable = async (client, transactionID) => {
     FOREIGN KEY (invoice_id) REFERENCES ${customerName}_transaction (id)
   );`;
   await client.query(sql);
-};
+}
+
+async function insertIntoCustomersTable(
+  client,
+  username,
+  name,
+  address,
+  phone_number,
+  email,
+  gstIN
+) {
+  const sql = `INSERT INTO ${username}_customers (name, address, phone_number, email, gstIN) VALUES (?, ?, ?, ?, ?)`;
+  const values = [name, address, phone_number, email, gstIN];
+  await client.query(sql, values);
+}
+
+async function insertIntoInvoicesTable(
+  client,
+  customerName,
+  customer_id,
+  transaction_id,
+  date_time,
+  amount_paid,
+  method_of_payment
+) {
+  const sql = `INSERT INTO ${customerName}_transaction (customer_id, transaction_id, date_time, amount_paid, method_of_payment) VALUES (?, ?, ?, ?, ?)`;
+  const values = [
+    customer_id,
+    transaction_id,
+    date_time,
+    amount_paid,
+    method_of_payment,
+  ];
+  await client.query(sql, values);
+}
+
+async function insertIntoInvoiceLinesTable(
+  client,
+  transactionID,
+  invoice_id,
+  product_id,
+  product_name,
+  price,
+  quantity,
+  amount,
+  tax,
+  taxed_amount
+) {
+  const sql = `INSERT INTO ${transactionID}_invoicelines (invoice_id, product_id, product_name, price, quantity, amount, tax, taxed_amount) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+  const values = [
+    invoice_id,
+    product_id,
+    product_name,
+    price,
+    quantity,
+    amount,
+    tax,
+    taxed_amount,
+  ];
+  await client.query(sql, values);
+}
 
 const main = async () => {
   const client = new Client({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'password',
-    database: 'my_database',
+    host: "localhost",
+    user: "postgres",
+    password: "password",
+    database: "my_database",
     port: 5432,
   });
 
-  await client.connect();
-  await createCustomersTable(client, 'username');
-  await createInvoicesTable(client, 'customerName');
-  await createInvoiceLinesTable(client, 'transactionID');
+  // await client.connect();
+  // await createCustomersTable(client, "username");
+  // await createInvoicesTable(client, "customerName");
+  // await createInvoiceLinesTable(client, "transactionID");
   await client.end();
 };
 
-main();
+module.exports = {
+  invoiceRouter,
+  createCustomersTable,
+  createInvoicesTable,
+  createInvoiceLinesTable,
+  insertIntoCustomersTable,
+  insertIntoInvoicesTable,
+  insertIntoInvoiceLinesTable,
+};
